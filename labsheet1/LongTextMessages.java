f59851858856b902149bfc17ccf00406307eef82
unknown <Anna.Kovalenko@students.ittralee.ie>
2023-09-08 14:17:07 +0100

    Created 2 classes

---
+++ labsheet1/LongTextMessages.java
@@ -0,0 +1,15 @@
+package labsheet1;
+
+public class LongTextMessages {
+    public static void main(String[] args) {
+        System.out.println("Macavity’s a Mystery Cat: he’s called " +
+                "the Hidden Paw \n"+
+                "For he’s the master criminal who "+
+                "can defy the Law.\n"+
+                "He’s the bafflement of Scotland Yard, "+
+                "the Flying Squad’s despair:\n"+
+                "For when they reach the scene of crime "+
+                "— Macavity’s not there!\n\n"+
+                "\t\t\t\tT.S. Eliot 1888-1965 ");
+    }
+}
